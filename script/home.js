0b25afb2d7efc51d127e0029a7bdbed641ba4536
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-02-22 17:19:29 +0600

    find total cards number and update Total Card Number Section

--- script/home.js
+++ script/home.js
@@ -1,1 +1,15 @@
-console.log("JS Connected");
+let totalJob = [];
+let totalSelectedInterview = [];
+let totalRejectedInterview = [];
+
+const total = document.getElementById("totalCount");
+const totalInterview = document.getElementById("interview-count");
+const totalRejected = document.getElementById("rejected-count");
+
+const totalJobs = document.getElementById("card");
+
+function calculateTotal() {
+  total.innerText = totalJobs.children.length;
+}
+
+calculateTotal();
