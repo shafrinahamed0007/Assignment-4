5577f55cf72414e6f4f959f316f2985eb0675a31
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-02-23 10:49:44 +0600

    Finished toggle button color change

--- script/home.js
+++ script/home.js
@@ -6,10 +6,37 @@ const total = document.getElementById("totalCount");
 const totalInterview = document.getElementById("interview-count");
 const totalRejected = document.getElementById("rejected-count");
 
+// Actionable Toggle Button
+const btnAll = document.getElementById("btn-all");
+const btnInterview = document.getElementById("btn-interview");
+const btnRejected = document.getElementById("btn-rejected");
+
 const totalJobs = document.getElementById("card");
 
-function calculateTotal() {
+function totalCalculate() {
   total.innerText = totalJobs.children.length;
 }
 
-calculateTotal();
+totalCalculate();
+
+// toggle button function
+function toggleStyle(id) {
+  // remove bg color & text color
+  btnAll.classList.remove("bg-[#3B82F6]", "text-white");
+  btnInterview.classList.remove("bg-white", "text-white");
+  btnRejected.classList.remove("bg-white", "text-white");
+
+  // add new bg-color and text color
+  btnAll.classList.add("bg-white", "text-black");
+  btnInterview.classList.add("bg-white", "text-black");
+  btnRejected.classList.add("bg-white", "text-black");
+ 
+
+  const selectedBtn = document.getElementById(id);
+  selectedBtn.classList.remove("bg-white", "text-black");
+  selectedBtn.classList.add("bg-[#3b82f6]", "text-white");
+
+  
+
+
+}
